fbf1948449802b1e81029b3f3f6e5fd8ad020ed9
rauladrian_ <diasraul54@gmail.com>
2024-05-25 18:05:06 +0530

    Added call from the generate pdf service to the create pdf service

--- src/main/java/com/freightfox/pdfgenerator/service/serviceImpl/GeneratePDFServiceImpl.java
+++ src/main/java/com/freightfox/pdfgenerator/service/serviceImpl/GeneratePDFServiceImpl.java
@@ -1,13 +1,20 @@
 package com.freightfox.pdfgenerator.service.serviceImpl;
 
 import com.freightfox.pdfgenerator.DTO.Invoice;
+import com.freightfox.pdfgenerator.service.CreatePDFService;
 import com.freightfox.pdfgenerator.service.GeneratePDFService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GeneratePDFServiceImpl implements GeneratePDFService {
+
+    @Autowired
+    CreatePDFService createPDFService;
+
     @Override
     public String generatePDF(Invoice invoice) {
+        createPDFService.createPDF(invoice);
         return invoice.getBuyer();
     }
 }
